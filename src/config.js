cf21573fa28626a37deb0c2e624c6e7c1c63da67
muguMugunthan <50227435+muguMugunthan@users.noreply.github.com>
2020-09-24 15:36:31 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,9 @@
+  
+  // Config starter code
+  import { createChatBotMessage } from "react-chatbot-kit";
+  
+  const config = {
+    initialMessages: [createChatBotMessage(`Welcome to TEEKS 2020 ,How may i help you`)]
+  }
+  
+  export default config
